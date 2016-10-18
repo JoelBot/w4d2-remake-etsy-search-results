c1cb5b15e5cabf0d795ca0ebd2e5857ff51c7021
JoelBot <Joel.Lonneker@gmail.com>
2016-10-18 14:18:57 -0400

    first box (13) created via JS

---
+++ js/global.js
@@ -0,0 +1,7 @@
+var result = document.createElement('div')
+result.classList = 'col-sm-3'
+var resultImage = document.createElement('img')
+result.innerHTML = '13'
+console.dir(result)
+
+document.getElementById('searchResultsSection').appendChild(result)
